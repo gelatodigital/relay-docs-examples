23453f11c46f04ed0c65b13dc99f96383bad8715
Sasha <sasha@gelato.digital>
2022-09-07 18:02:15 +0200

    chore: updated README and added some solidity comments

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Relay Docs Counter
+# Relay Docs Examples
 
-Solidity Contracts + Tests.
+- This repo contains the contracts used for code examples in the [Relay SDK docs](https://docs.gelato.network/developer-products/gelato-relay-sdk).
+- This repo is intended as something you can clone and get started with local testing straight away, but bear in mind, this is a WIP.
+- Furthermore, the ultimate aim is to have all code examples from the docs, working, with tests, so that you, as a developer, can get started immediately with just one `git clone` command.
--- contracts/MyDummyWallet.sol
+++ contracts/MyDummyWallet.sol
@@ -10,15 +10,24 @@ import {
     SafeERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+// Once GelatoRelayContext is inherited, this contract can use `onlyGelatoRelay` for safety
+// and the GelatoRelay address is known automatically.
 contract MyDummyWallet is GelatoRelayContext {
+    // emitting an event for testing purposes
     event LogSendToFriend(address indexed to, uint256 amount);
 
+    // this function uses this contract's mock ERC-20 balance to send
+    // an _amount of tokens to the _to address
     function sendToFriend(
         address _token,
         address _to,
         uint256 _amount
     ) external onlyGelatoRelay {
         // payment to Gelato
+        // NOTE: be very careful here!!
+        // if you do not use the onlyGelatoRelay modifier,
+        // anyone could encode themselves as the fee collector
+        // in the low-level data and drain tokens from this contract.
         _transferRelayFee();
 
         // transfer of ERC-20 tokens
